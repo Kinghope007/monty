e78d0022aef2dbf829adf80bd93be159ad01a6fe
Kinghope007 <emmanuelhope39@gmail.com>
2023-03-23 03:25:04 -0700

    error message

--- monty.c
+++ monty.c
@@ -1,4 +1,30 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h.
+/**
+ * error_usage - print usage message and exits
+ *
+ * return: nothing
+ */
+void error_usage(void)
+{
+	fprintf(stderr, "USAGE: minty file\n");
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * file_error - print file error messsage and exits
+ * @av: argv given by main
+ *
+ * Return: nothing
+ */
+void file_error(char *av)
+{
+	fprintf(stderr, "Error: can't open file %s\n", av);
+	exit(EXIT_FAILURE);
+}
+
 global_var var_global;
 /**
  * main - driver function for monty program
